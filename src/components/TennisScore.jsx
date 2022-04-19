86c27125e5896050b765de4c811cfa7abe559232
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-04-19 11:50:39 +0200

    feat(initialState) : ajout des valeurs initials

---
+++ src/components/TennisScore.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const initialState = {
+  player1: 0,
+  player2: 0,
+  advantage: null,
+  winner: null,
+  playing: true
+
+}
+
+const TennisScore = () => {
+  return (
+    <div>TennisScore</div>
+  )
+}
+
+export default TennisScore
